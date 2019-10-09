659f3486d4cc546f759161e15c90ab6ce59fc241
Anthony Felts <me@anthonyfelts.com>
2019-10-09 21:28:13 +0000

    added rofi config

--- sync.sh
+++ sync.sh
@@ -1,6 +1,7 @@
 cp ~/.config/i3/config ./i3config
 cp ~/.config/i3blocks/config ./i3blocksconfig
 cp ~/.config/i3status/config ./i3statusconfig
+cp ~/.config/rofi/config.rasi ./roficonfig
 cp ~/.Xdefaults ./Xdefaults
 cp ~/.config/kitty/kitty.conf ./kittyconfig
 cp ~/.zshrc ./zshrc
