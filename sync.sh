620ed87db89a93ba0aea2a77ff080d76ea483341
Anthony Felts <me@anthonyfelts.com>
2019-10-09 14:27:18 +0000

    10/09/19 14:27:18

--- sync.sh
+++ sync.sh
@@ -2,4 +2,6 @@ cp ~/.config/i3/config ./i3config
 cp ~/.config/i3blocks/config ./i3blocksconfig
 cp ~/.Xdefaults ./Xdefaults
 cp ~/.config/kitty/kitty.conf ./kittyconfig
+cp ~/.zshrc ./zshrc
+cp ~/.vimrc ./vimrc
 git commit -am "`date +"%D %T"`"
